b855ae504f09b9c8889617df09249d8d67bca913
Ellis-Ayikwei <ellisarmahayikwei@gmail.com>
2024-03-07 19:08:13 +0000

    created the models folder and the test

---
+++ models/base_model.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""defines  the base model class"""
+
+from uuid import uuid4
+from datetime import datetime
+
+class BaseModel:
+    """represents the BaseModel Class of the Airbnb clone project."""
+    
+    def __init__(self):
+        """initialize a new BaseModel"""
+        self.id = str(uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def __str__(self):
+        """returns a string representation of the instance"""
+        return f"[ {self.__class__.__name__} ] ({self.id}) {self.__dict__}"
+
+    def save(self):
+        """updates the public instance attribute updated_at with the current datetime"""
+        self.updated_at = datetime.now()
+        
+        
+        
+    def to_dict(self):
+        """A function that returns the dic"""
+        ret_dict = self.__dict__.copy()
+        ret_dict["__class__"] = self.__class__.__name__
+        ret_dict["created_at"] = ret_dict["created_at"].isoformat()  
+        ret_dict["updated_at"] = ret_dict["updated_at"].isoformat()  
+        return ret_dict

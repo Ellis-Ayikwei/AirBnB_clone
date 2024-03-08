74ce10b3b0835bb8319f612a5fa2f257517bc442
Ellis-Ayikwei <ellisarmahayikwei@gmail.com>
2024-03-08 01:35:28 +0000

    fixed the file reaload in the Filestorage clas to assign the loaded json data to the filestrorage.__objects

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -6,36 +6,36 @@ class FileStorage:
     """Represent an abstracted storage engine.
 
 Attributes:
-    __file_path (str): The name of the file to save objects to.
-    __objects (dict): A dictionary of instantiated objects.
+    ___file_path (str): The name of the file to save objects to.
+    ___objects (dict): A dictionary of instantiated objects.
 """
 
 
 
-    _file_path = "file.json"
-    _objects = {}
+    __file_path = "file.json"
+    __objects = {}
 
 
     def all(self):
-        """ returns the dictionary __objects """
-        return FileStorage._objects
+        """ returns the dictionary ___objects """
+        return FileStorage.__objects
 
     def new(self, obj):
-        """ sets in __objects the obj with key <obj class name>.id """
+        """ sets in ___objects the obj with key <obj class name>.id """
         key = obj.__class__.__name__ + "." + obj.id
-        FileStorage._objects[key] = obj
+        FileStorage.__objects[key] = obj
 
     def save(self):
-        """ saves objects into json file thats the __file_path"""
-        odict = FileStorage._objects
+        """ saves objects into json file thats the ___file_path"""
+        odict = FileStorage.__objects
         obj_dict = {obj: odict[obj].to_dict() for obj in odict.keys()}
-        with open(FileStorage._file_path, 'w') as f:
+        with open(FileStorage.__file_path, 'w') as f:
             json.dump(obj_dict, f)
 
     def reload(self):
-        """Deserialize the JSON file __file_path to __objects, if it exists."""
+        """Deserialize the JSON file ___file_path to ___objects, if it exists."""
         try:
-            with open(FileStorage._file_path) as f:
-                json.load(f)  
+            with open(FileStorage.__file_path) as f:
+                FileStorage.__objects = json.load(f)  
         except FileNotFoundError:
             return

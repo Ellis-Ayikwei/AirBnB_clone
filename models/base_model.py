087d1bd5f671e858728e124e41f282a67daaadff
Alphajay1 <ayakojamesty@gmail.com>
2024-03-09 06:24:55 -0800

    committing changes before switching branches

---
+++ models/base_model.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+"""
+Contains class BaseModel
+"""
+
+from datetime import datetime
+from models.engine.file_storage import FileStorage
+import uuid
+
+time = "%Y-%m-%dT%H:%M:%S.%f"
+
+
+class BaseModel:
+    """The BaseModel class from which future models will be derived"""
+
+    def __init__(self, *args, **kwargs):
+        """Initialization of the base model
+
+        Args:
+            *args(any) : not used
+            **kwargs(dict) : key/value pairs of attributes
+
+        """
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != "__class__":
+                    setattr(self, key, value)
+            if hasattr(self, "created_at") and type(self.created_at) is str:
+                self.created_at = datetime.strptime(kwargs["created_at"], time)
+            if hasattr(self, "updated_at") and type(self.updated_at) is str:
+                self.updated = datetime.strptime(kwargs["updated_at"], time)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            models.storage.new(self)
+
+    def __str__(self):
+        """Returns string representation of BaseModel class"""
+        return f"{self.__class__.__name__} ({self.id}){self.__dict__}"
+
+    def save(self):
+        """Updates the attribute 'updated_at' with the current datetime"""
+        self.updated_at = datetime.now()
+        models.storage.save()
+
+    def to_dict(self):
+        """Returns a new dictionary containing all keys/values
+        of '__dict__' of the instance"""
+        new_dict = self.__dict__.copy()
+        if "created_at" in new_dict:
+            new_dict["created_at"] = new_dict["created_at"].isoformat()
+        if "updated_at" in new_dict:
+            new_dict["updated_at"] = new_dict["updated_at"].isoformat()
+        new_dict["__class__"] = self.__class__.__name__
+        return new_dict

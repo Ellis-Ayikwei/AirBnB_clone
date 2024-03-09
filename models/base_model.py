dc953579a6460a1a43a34c6119ac6a1849727e7a
Alphajay1 <ayakojamesty@gmail.com>
2024-03-09 07:35:52 -0800

    AirBnB

--- models/base_model.py
+++ models/base_model.py
@@ -1,61 +1,10 @@
 #!/usr/bin/python3
-"""defines  the base model class"""
-
-from uuid import uuid4
-from datetime import datetime
-import models
-
-class BaseModel:
-    """represents the BaseModel Class of the Airbnb clone project."""
-    
-    def __init__(self, *args, **kwargs):
-        """initialize a new BaseModel
-        
-        Args:
-           *args(any) : not used 
-           **kwargs(dict) : Key/value pairs of attributes
-        
-        """
-        timeformat = "%Y-%m-%dT%H:%M:%S.%f"
-        self.id = str(uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
-        if len(kwargs) != 0:
-            for key,value in kwargs.items():
-                if key == "created_at" or key =="updated_at":
-                    self.__dict__[key] = datetime.strptime(value,timeformat)
-        else:
-            self.id = str(uuid4())  
-            self.created_at = datetime.now()
-            models.storage.new(self)
-        
-
-    def __str__(self):
-        """returns a string representation of the instance"""
-        return f"[ {self.__class__.__name__} ] ({self.id}) {self.__dict__}"
-
-    def save(self):
-        """updates the public instance attribute updated_at with the current datetime"""
-        self.updated_at = datetime.now()
-        models.storage.save()
-        
-        
-        
-        
-    def to_dict(self):
-        """A function that returns the dic"""
-        ret_dict = self.__dict__.copy()
-        ret_dict["created_at"] = ret_dict["created_at"].isoformat()  
-        ret_dict["__class__"] = self.__class__.__name__
-        ret_dict["updated_at"] = ret_dict["updated_at"].isoformat()  
-        return ret_dict
-=======
 """
 Contains class BaseModel
 """
 
 from datetime import datetime
-from models.engine.file_storage import FileStorage
+import models
 import uuid
 
 time = "%Y-%m-%dT%H:%M:%S.%f"
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,55 +1,19 @@
 #!/usr/bin/python3
-<<<<<<< HEAD
-""" defines a FileStroage class """
-import json
-from models.base_model import BaseModel
-
-class FileStorage:
-    """Represent an abstracted storage engine.
-
-Attributes:
-    ___file_path (str): The name of the file to save objects to.
-    ___objects (dict): A dictionary of instantiated objects.
-"""
-
-
-
-    __file_path = "file.json"
-    __objects = {}
-
-
-    def all(self):
-        """ returns the dictionary ___objects """
-        return FileStorage.__objects
-
-    def new(self, obj):
-        """ sets in ___objects the obj with key <obj class name>.id """
-        key = obj.__class__.__name__ + "." + obj.id
-        FileStorage.__objects[key] = obj
-
-    def save(self):
-        """ saves objects into json file thats the ___file_path"""
-        pdict = FileStorage.__objects
-        obj_dict = {obj: pdict[obj].to_dict()
-                    for obj in pdict.keys() 
-                    if isinstance(pdict[obj], BaseModel)}
-        with open(FileStorage.__file_path, 'w') as f:
-            json.dump(obj_dict, f)
-
-    def reload(self):
-        """Deserialize the JSON file ___file_path to ___objects, if it exists."""
-        try:
-            with open(FileStorage.__file_path) as f:
-                FileStorage.__objects = json.load(f)  
-        except FileNotFoundError:
-            return
-=======
 """
 Contains class 'FileStorage'
 """
 
 import json
+from models.amenity import Amenity
 from models.base_model import BaseModel
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+
+classes = {"Amenity": Amenity, "BaseModel": BaseModel, "City": City,
+           "Place": Place, "Review": Review, "State": State, "User": User}
 
 
 class FileStorage:
@@ -93,4 +57,3 @@ class FileStorage:
                 self.__objects[key] = classes[new_obj[key]["__class__"]](**new_obj[key])
         except FileNotFoundError:
             pass
->>>>>>> james/AirBnB

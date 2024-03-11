2d3b0e100411bcd58ac9a13c68689b5f46e53997
Alphajay1 <ayakojamesty@gmail.com>
2024-03-11 07:10:23 -0700

    console.py

--- console.py
+++ console.py
@@ -6,6 +6,12 @@ import re
 from models import storage
 from models.base_model import BaseModel
 from shlex import split
+from models.user import User
+from models.state import State
+from models.city import City
+from models.place import Place
+from models.amenity import Amenity
+from models.review import Review
 
 def parse(arg):
     """Tokenizes the input string"""
